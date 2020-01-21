50c5f8489b78878ed324afdbc4801b1d672591a3
czooosnekplow <mrojek@czooosnek.cloud>
2020-01-21 21:11:45 +0100

    script init

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-const noteCard = document.querySelector('p')
+const noteCard = document.querySelectorAll('p')
 const noteAddForm = document.querySelector('#add-note')
+const noteContentInput = document.querySelector('#content-note')
+const notes = {
+    
+}
+for (i=0;i<=20;i++) {
+    const objectLength = Object.keys(notes).length
+    localStorage.getItem(`Note${(objectLength + 1)}`)
+}
+noteAddForm.addEventListener('submit', function(e, note) {
+    e.preventDefault()
+    const objectLength = Object.keys(notes).length
+    if (noteContentInput.value != "") {
+        notes[`Note${(objectLength + 1)}`] = noteContentInput.value
+        localStorage.setItem(`Note${(objectLength + 1)}`, noteContentInput.value)
+        noteContentInput.value = ''
+    }
+    if (notes.lenght > 0) {
+        document.removeChild(noteCard);
+    }
+    console.log(notes)
+})
