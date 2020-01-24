23b6bffcbfd291f32011ba8ce89fb15bb3f02378
czooosnekplow <mrojek@czooosnek.cloud>
2020-01-24 23:44:13 +0100

    added posibility to edit notes + opt

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ if (notesJSON !== null) {
     notes = JSON.parse(notesJSON)
 }
 
+const updateLocalStorage = function () {
+    localStorage.setItem('notes', JSON.stringify(notes))
+    localStorage.getItem('notes')
+}
+
 const showNotes = function () {
     notes.forEach(function (note, id) {
         const noteContainer = document.createElement('p')
         noteContainer.className = `d-flex flex-column flex-wrap note`
-        noteContainer.id = id
         noteContainer.textContent = note.content
+        noteContainer.id = id
         document.querySelector('.content').appendChild(noteContainer)
     })
 }
@@ -44,13 +49,39 @@ const noteCard = document.querySelectorAll('.note')
 
 const deleteNote = function () {
     noteCard.forEach(function (target) {
-        target.addEventListener('click', function (e) {
+        target.addEventListener('contextmenu', function (e) {
             notes.splice(target.id,1)
             refreshNotes()
             showNotes()
-            localStorage.setItem('notes', JSON.stringify(notes))
-            localStorage.getItem('notes')
+            updateLocalStorage()
         })
     })
 }
 deleteNote()
+
+const editNote = function () {
+    noteCard.forEach(function (target) {
+        target.addEventListener('click', function (e) {
+            let notesArray = notes
+            const modal = document.querySelector('#editModal')
+            const modalTextArea = document.querySelector('#editNoteValue')
+            const buttonSaveNote = document.querySelector('#saveNote')
+            const containerId = this.id
+            $("#editModal").modal('toggle')
+            modalTextArea.value = e.target.innerHTML
+            buttonSaveNote.addEventListener('click', function (e) {
+                console.log(notesArray.length)
+                const pushChanges = function () {
+                    notesArray[containerId] = {
+                        content: modalTextArea.value,
+                    }
+                }
+                pushChanges()
+                updateLocalStorage()
+                refreshNotes()
+                showNotes()
+            })
+        })
+    })
+}
+editNote()
